954afcfe1949283ee0c22b891ba638af6922e5e0
h+ <serdar@justserdar.dev>
2025-02-21 18:33:49 +0100

    feat: CreemCheckoutCallbackHandler and ReturnParam type

--- src/runtime/server/utils/creem/verify.ts
+++ src/runtime/server/utils/creem/verify.ts
@@ -1,9 +1,12 @@
 import * as crypto from 'node:crypto'
+import type {
+  RedirectParams,
+} from '../../../types/creem/callback'
 
-export function generateSignature(payload: string, secret: string): string {
-  const computedSignature = crypto
-    .createHmac('sha256', secret)
-    .update(payload)
-    .digest('hex')
-  return computedSignature
+export function generateSignature(params: RedirectParams, apiKey: string): string {
+  const data = Object.entries(params)
+    .map(([key, value]) => `${key}=${value}`)
+    .concat(`salt=${apiKey}`)
+    .join('|')
+  return crypto.createHash('sha256').update(data).digest('hex')
 }
